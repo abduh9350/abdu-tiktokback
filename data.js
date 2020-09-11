8e768da654fa104c2c64b9031e97afa3acd52e85
Abdu Jemal <abduh9350@gmail.com>
2020-09-11 07:22:29 -0400

    make it better

--- data.js
+++ data.js
@@ -1,9 +1,23 @@
 const data = [
   {
+    url:
+      "https://www.pexels.com/video/a-man-working-with-two-laptops-while-looking-at-notes-stick-on-the-board-behind-3253737/",
     channel: "Abdu",
+    description: "tiktok video 1",
+    song: "new music",
+    likes: 111,
+    messages: 222,
+    shares: 333,
   },
   {
+    url:
+      "https://www.pexels.com/video/a-man-working-with-two-laptops-while-looking-at-notes-stick-on-the-board-behind-3253737/",
     channel: "Jemal",
+    description: "tiktok video 2",
+    song: "old music",
+    likes: 112,
+    messages: 223,
+    shares: 334,
   },
 ];
 
--- server.js
+++ server.js
@@ -1,10 +1,46 @@
 import express from "express";
 import mongoose from "mongoose";
 import Data from "./data.js";
+import Videos from "./dbModel.js";
 
 const app = express();
 const port = 9000;
 
+app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeaders("Access-Control-Allow-Origin", "*"),
+    res.setHeaders("Access-Control-Allow-Headers", "*"),
+    next();
+});
+
+const connection_url =
+  "mongodb+srv://admin:zer4bsK6fEQkvmN5@cluster0.lowqx.mongodb.net/abdu-tiktok?retryWrites=true&w=majority";
+
+mongoose.connect(connection_url, {
+  useNewUrlPalser: true,
+  useCreateIndex: true,
+  useUnifieldTopology: true,
+});
+
 app.get("/", (req, res) => res.status(200).send("hello world"));
 app.get("/v1/posts", (req, res) => res.status(200).send(Data));
+app.get("/v2/posts", (req, res) => {
+  Videos.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
+app.post("/v2/posts", (req, res) => {
+  const dbVideos = req.body;
+  Videos.create(dbVideos, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
 app.listen(port, () => console.log(`listening on localhost:${port}`));
